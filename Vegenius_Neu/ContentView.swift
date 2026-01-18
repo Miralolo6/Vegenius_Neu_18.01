a31a5efc515fbbd3d58d3ad1def8a7c0191944e4
START Amira <mira.babina@icloud.com>
2026-01-18 10:27:19 +0100

    Initial Commit

---
+++ Vegenius_Neu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vegenius_Neu
+//
+//  Created by TA604 on 18.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vegenius_Neu/Vegenius_NeuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vegenius_NeuApp.swift
+//  Vegenius_Neu
+//
+//  Created by TA604 on 18.01.26.
+//
+
+import SwiftUI
+
+@main
+struct Vegenius_NeuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
